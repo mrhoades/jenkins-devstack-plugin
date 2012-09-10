b6817aaf437f305692c21b7f9168304d58254b8e
Tim Miller <tim.miller.0@gmail.com>
2012-09-10 18:13:52 +0000

    hook up openstack client, list instances

--- models/devstack.rb
+++ models/devstack.rb
@@ -1,7 +1,8 @@
+require 'openstack'
 
 class Devstack < Jenkins::Tasks::Builder
 
-  attr_reader :os_username, :os_password, :os_tenant_name
+  attr_reader :os_username, :os_password, :os_tenant_name, :os_auth_url
 
   display_name 'Devstack Deploy'
 
@@ -11,7 +12,15 @@ class Devstack < Jenkins::Tasks::Builder
 
   def perform(build, launcher, listener)
     listener.info 'hello, ' + os_username
-    # do stuff
+
+    os = OpenStack::Connection.create(
+      :username => os_username,
+      :api_key => os_password,
+      :authtenant => os_tenant_name,
+      :auth_url => os_auth_url,
+      :service_type => "compute")
+
+    listener.info os.servers.inspect
   end
 
 end
